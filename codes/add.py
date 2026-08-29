243c2d7c779a8e63a5430ab0e10385e045a469e2
Bowen <bwyuchi@gmail.com>
2026-08-29 18:15:01 +0800

    添加 TileLang 向量加法示例

---
+++ codes/add.py
@@ -0,0 +1,29 @@
+import torch
+import tilelang.language as T
+from tilelang import jit
+
+@jit
+def add(N: int, block_size: int = 128, dtype=torch.float32):
+    @T.prim_func
+    def add_kernel(A: T.Tensor((N,), dtype), B: T.Tensor((N,), dtype), C: T.Tensor((N,), dtype)): # pyright: ignore[reportInvalidTypeForm]
+        with T.Kernel(T.cdiv(N, block_size), threads=block_size) as bx:
+            for i in T.Parallel(block_size):
+                idx = bx * block_size + i
+                if idx < N:
+                    C[idx] = A[idx] + B[idx]
+    return add_kernel
+
+DEVICE='mps'
+N =270
+a = torch.randn((N), device=DEVICE)
+b = torch.randn((N,), device=DEVICE)
+c = torch.empty((N,), device=DEVICE)
+
+my_add = add(N)
+my_add(a, b, c)
+
+ref_c = a + b
+
+torch.testing.assert_close(c, ref_c)
+print('ref_c=', ref_c)
+print('c=', c)
